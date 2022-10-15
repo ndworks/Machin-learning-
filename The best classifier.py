6e88b1423598749c3d2d1f0b475eac3e5be4c35f
Navneet Devare <111160753+ndworks@users.noreply.github.com>
2022-10-15 13:41:12 +0530

    Add files via upload

---
+++ The best classifier.py
@@ -0,0 +1,511 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # classification with python
+
+# In[1]:
+
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### About dataset
+
+# This dataset is about past loans. The __Loan_train.csv__ data set includes details of 346 customers whose loan are already paid off or defaulted. It includes following fields:
+# 
+# | Field          | Description                                                                           |
+# |----------------|---------------------------------------------------------------------------------------|
+# | Loan_status    | Whether a loan is paid off on in collection                                           |
+# | Principal      | Basic principal loan amount at the                                                    |
+# | Terms          | Origination terms which can be weekly (7 days), biweekly, and monthly payoff schedule |
+# | Effective_date | When the loan got originated and took effects                                         |
+# | Due_date       | Since it’s one-time payoff schedule, each loan has one single due date                |
+# | Age            | Age of applicant                                                                      |
+# | Education      | Education of applicant                                                                |
+# | Gender         | The gender of applicant                                                               |
+
+# Lets download the dataset
+
+# In[2]:
+
+
+get_ipython().system('wget -O loan_train.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_train.csv')
+
+
+# ### Load Data From CSV File  
+
+# In[3]:
+
+
+df = pd.read_csv('loan_train.csv')
+df.head()
+
+
+# In[4]:
+
+
+df.shape
+
+
+# ### Convert to date time object 
+
+# In[5]:
+
+
+df['due_date'] = pd.to_datetime(df['due_date'])
+df['effective_date'] = pd.to_datetime(df['effective_date'])
+df.head(500)
+
+
+# # Data visualization and pre-processing
+# 
+# 
+
+# Let’s see how many of each class is in our data set 
+
+# In[6]:
+
+
+df['loan_status'].value_counts()
+
+
+# 260 people have paid off the loan on time while 86 have gone into collection 
+# 
+
+# Lets plot some columns to underestand data better:
+
+# In[7]:
+
+
+# notice: installing seaborn might takes a few minutes
+get_ipython().system('conda install -c anaconda seaborn -y')
+
+
+# In[8]:
+
+
+import seaborn as sns
+
+bins = np.linspace(df.Principal.min(), df.Principal.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'Principal', bins=bins, ec="k")
+
+g.axes[-1].legend()
+plt.show()
+
+
+# In[9]:
+
+
+bins = np.linspace(df.age.min(), df.age.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'age', bins=bins, ec="k")
+
+g.axes[-1].legend()
+plt.show()
+
+
+# # Pre-processing:  Feature selection/extraction
+
+# ### Lets look at the day of the week people get the loan 
+
+# In[10]:
+
+
+df['dayofweek'] = df['effective_date'].dt.dayofweek
+bins = np.linspace(df.dayofweek.min(), df.dayofweek.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'dayofweek', bins=bins, ec="k")
+g.axes[-1].legend()
+plt.show()
+
+
+# We see that people who get the loan at the end of the week dont pay it off, so lets use Feature binarization to set a threshold values less then day 4 
+
+# In[11]:
+
+
+df['weekend'] = df['dayofweek'].apply(lambda x: 1 if (x>3)  else 0)
+df.head()
+
+
+# ## Convert Categorical features to numerical values
+
+# Lets look at gender:
+
+# In[12]:
+
+
+df.groupby(['Gender'])['loan_status'].value_counts(normalize=True)
+
+
+# 86 % of female pay there loans while only 73 % of males pay there loan
+# 
+
+# Lets convert male to 0 and female to 1:
+# 
+
+# In[13]:
+
+
+df['Gender'].replace(to_replace=['male','female'], value=[0,1],inplace=True)
+df
+
+
+# ## One Hot Encoding  
+# #### How about education?
+
+# In[14]:
+
+
+df.groupby(['education'])['loan_status'].value_counts(normalize=True)
+
+
+# #### Feature befor One Hot Encoding
+
+# In[15]:
+
+
+df[['Principal','terms','age','Gender','education']].head()
+
+
+# #### Use one hot encoding technique to conver categorical varables to binary variables and append them to the feature Data Frame 
+
+# In[16]:
+
+
+Feature = df[['Principal','terms','age','Gender','weekend']]
+Feature = pd.concat([Feature,pd.get_dummies(df['education'])], axis=1)
+Feature.drop(['Master or Above'], axis = 1,inplace=True)
+Feature.head()
+
+
+# ### Feature selection
+
+# Lets define feature sets, X:
+
+# In[17]:
+
+
+X = Feature
+X[0:5]
+
+
+# What are our lables?
+
+# In[18]:
+
+
+y = df['loan_status'].values
+y[0:5]
+
+
+# ## Normalize Data 
+
+# Data Standardization give data zero mean and unit variance (technically should be done after train test split )
+
+# In[19]:
+
+
+X= preprocessing.StandardScaler().fit(X).transform(X)
+X[0:5]
+
+
+# # Classification 
+
+# Now, it is your turn, use the training set to build an accurate model. Then use the test set to report the accuracy of the model
+# You should use the following algorithm:
+# - K Nearest Neighbor(KNN)
+# - Decision Tree
+# - Support Vector Machine
+# - Logistic Regression
+# 
+# 
+# 
+# __ Notice:__ 
+# - You can go above and change the pre-processing, feature selection, feature-extraction, and so on, to make a better model.
+# - You should use either scikit-learn, Scipy or Numpy libraries for developing the classification algorithms.
+# - You should include the code of the algorithm in the following cells.
+
+# # K Nearest Neighbor(KNN)
+# Notice: You should find the best k to build the model with the best accuracy.  
+# **warning:** You should not use the __loan_test.csv__ for finding the best k, however, you can split your train_loan.csv into train and test to find the best __k__.
+
+# In[20]:
+
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+X_train
+
+
+# In[21]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+Ks = 10
+mean_acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+ConfustionMx = [];
+for n in range(1,Ks):
+    
+    #Train Model and Predict  
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat=neigh.predict(X_test)
+    mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+
+    
+    std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+mean_acc #print accuracy array for eack k
+
+
+# In[22]:
+
+
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1) 
+
+
+# # Decision Tree
+
+# In[23]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+df
+
+
+# In[24]:
+
+
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+drugTree # it shows the default parameters
+drugTree.fit(X_train,y_train)
+predTree = drugTree.predict(X_test)
+
+
+# In[25]:
+
+
+#INSTALLATIONS TO VIEW THE DECISION TREE
+get_ipython().system('conda install -c conda-forge pydotplus -y')
+ 
+
+
+
+
+# In[26]:
+
+
+get_ipython().system('conda install -c conda-forge python-graphviz -y')
+
+
+# In[27]:
+
+
+from sklearn.externals.six import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
+get_ipython().run_line_magic('matplotlib', 'inline')
+dot_data = StringIO()
+filename = "drugtree.png"
+featureNames = df.columns[3: 11]
+targetNames = df["loan_status"].unique().tolist()
+out=tree.export_graphviz(drugTree,feature_names=featureNames, out_file=dot_data, class_names= np.unique(y_train), filled=True,  special_characters=True,rotate=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png(filename)
+img = mpimg.imread(filename)
+plt.figure(figsize=(100, 200))
+plt.imshow(img,interpolation='nearest')
+
+
+# # Support Vector Machine
+
+# In[28]:
+
+
+from sklearn import svm
+clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, y_train) 
+
+
+# In[29]:
+
+
+yhatsvm = clf.predict(X_test)
+yhatsvm [0:5]
+
+
+# In[32]:
+
+
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    # Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat)
+np.set_printoptions(precision=2)
+
+print (classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=df["loan_status"].unique().tolist(),normalize= False,  title='Confusion matrix')
+
+
+# # Logistic Regression
+
+# In[33]:
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
+LR
+
+
+# In[34]:
+
+
+yhatl = LR.predict(X_test)
+yhatl
+
+
+# In[36]:
+
+
+yhat_prob = LR.predict_proba(X_test)
+
+
+# # Model Evaluation using Test set
+
+# In[37]:
+
+
+from sklearn.metrics import jaccard_similarity_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import log_loss
+
+
+# First, download and load the test set:
+
+# In[39]:
+
+
+get_ipython().system('wget -O loan_test.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_test.csv')
+
+
+# ### Load Test set for evaluation 
+
+# In[40]:
+
+
+test_df = pd.read_csv('loan_test.csv')
+test_df.head()
+
+
+# In[43]:
+
+
+#test_df.groupby(['Gender'])['loan_status'].value_counts(normalize=True)
+print(test_df.dtypes)
+import numpy as np
+test_df['Gender'].replace(to_replace=['male','female'], value=[np.int64(0),np.int64(1)],inplace=True)
+
+test_df.groupby(['education'])['loan_status'].value_counts(normalize=True)
+Feature = test_df[['Principal','terms','age','Gender']]
+Feature = pd.concat([Feature,pd.get_dummies(test_df['education'])], axis=1)
+Feature.drop(['Master or Above'], axis = 1,inplace=True)
+Feature.head()
+X2 = Feature
+y2 = test_df['loan_status'].values
+X2= preprocessing.StandardScaler().fit(X2).transform(X2)
+
+
+# # Report
+# 
+
+# In[44]:
+
+
+predTree = drugTree.predict(X_test)
+print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_test, predTree))
+
+
+# In[47]:
+
+
+#for knn
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, yhat)
+print(cr)
+
+
+
+
+# In[48]:
+
+
+# for SVM
+classification_report(y_test,yhatsvm )
+print('SVM ACCY:', classification_report(y_test, yhatsvm))
+
+
+# In[49]:
+
+
+#for logit
+classification_report(y_test, yhatl)
+print('Logit', classification_report(y_test, yhatl))
+
